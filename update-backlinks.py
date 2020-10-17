495d978ffac14e35b5106e44b5184ac7e6045c3f
Alberto Bailoni <bailoni.alberto@gmail.com>
2020-10-17 09:41:19 +0200

    Change how backlinks are update

--- update-backlinks.py
+++ update-backlinks.py
@@ -16,15 +16,12 @@ update the note text.
 
 Change the `backlinks_header` to the name of the section you want to use.
 
-This script can be used with the standard Python 2.7 on macOS.
+This script can be used with the standard Python 3.7 on macOS.
 '''
 import sqlite3
 import subprocess
 import os
 import time
-import json
-import numpy as np
-import h5py
 import re
 
 import urllib.parse
@@ -34,14 +31,13 @@ from shutil import copyfile
 backlinks_header = '\n\n---\n### Backlinks'
 HOME = os.getenv('HOME', '')
 bear_db = os.path.join(HOME, 'Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data/database.sqlite')
-# bear_db = './databases/BAK.sqlite'
-old_mod_dates_file = './databases/mod_dates.h5'
 
 from datetime import datetime
 
 
 def main():
     # current date and time
+    # TODO: delete some of the old backups?
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
     BAK_db = bear_db.replace(".sqlite", "_{}.sqlite".format(dt_string))
@@ -52,63 +48,68 @@ def main():
     print("Read in ", time.time() - tick)
     # max_nb_notes = len(notes) + 1000
 
-    # Load modification dates and create new array:
-    if os.path.exists(old_mod_dates_file):
-        with h5py.File(old_mod_dates_file, 'r') as f:
-            old_modification_dates = f['data'][:]
-
-    else:
-        old_modification_dates = None
-
     # Loop over the notes:
     for counter, note in enumerate(notes):
         # Split Note text from backlinks
-        note_text = note['Text'].split(backlinks_header)[0]
-        backlinks = None
+        split_note = note['Text'].split(backlinks_header)
+        note_text = split_note[0]
+
+        # Get list of current backlinks:
+        nb_found_backlinks = 0
+        if len(split_note) == 1:
+            old_backlinks = []
+        elif len(split_note) == 2:
+            old_backlinks = get_current_backlinks(split_note[1])
+        else:
+            raise ValueError("Something went wrong while detecting backlinks in note: {}".format(note["Title"]))
+
+        backlinks = backlinks_header
 
         # Get notes this note is referenced by
         linked_by_notes = get_notes_linking_to(note['ID'])
-        links_in_note = get_note_links_in(note['ID'])
-        ID_links_in_node = [link["ID"] for link in links_in_note]
 
         # Make sure that we also match sub-links:
         re_note_title = re.compile(r"\[\[(" + re.escape(note['Title']) + r")(|.+)\]\]")
 
+        update_backlinks = False
         for linked_by_note in linked_by_notes:
-            # If note was not modified from the last run, skip it:
-            if old_modification_dates is not None:
-                if linked_by_note['ID'] < old_modification_dates.shape[0]:
-                    old_date = old_modification_dates[linked_by_note['ID']]
-                    new_date = linked_by_note['ModDate']
-                    if new_date == old_date:
-                        continue
-
             lb_note_text = linked_by_note['Text'].split(backlinks_header)[0]
             # Only add if link is in the text and if it is not already linked
             if re_note_title.search(lb_note_text) is not None:
                 re_lb_note_title = re.compile(r"\[\[(" + re.escape(linked_by_note['Title']) + r")(|.+)\]\]")
-                # if ('[[' + linked_by_note['Title'] + ']]') not in note_text:
                 if re_lb_note_title.search(note_text) is None:
-                    backlinks = backlinks_header if backlinks is None else backlinks
-                    # Add backlink
-                    backlinks += "\n- [[" + linked_by_note['Title'] + "]]"
+                    # Check if backlink was already there:
+                    # We will update the note only if at least one is different
+                    # (They will even be updated when their order change)
+                    if len(old_backlinks) <= nb_found_backlinks:
+                        update_backlinks = True
+                    elif old_backlinks[nb_found_backlinks] != linked_by_note['Title']:
+                        update_backlinks = True
 
-        if backlinks is not None:
-            print("Title updated note: {}".format(note['Title']))
-            note_text += backlinks
+                    # Add to current backlink list:
+                    backlinks += "\n- [[" + linked_by_note['Title'] + "]]"
+                    nb_found_backlinks += 1
+
+        # Check if we found some new ones:
+        if nb_found_backlinks != len(old_backlinks):
+            update_backlinks = True
+
+        if update_backlinks:
+            print("Backlinks updated in note: {}".format(note['Title']))
+            # If we did not found any link, remove the Backlinks section:
+            if nb_found_backlinks > 0:
+                note_text += backlinks
             update_note(note['UID'], note_text)
 
-    # # Read database again and save mod-dates to array for next run:
-    # notes = get_all_notes()
-    # for counter, note in enumerate(notes):
-    #     if counter == 0:
-    #         max_nb_notes = note['ID'] + 1
-    #         new_modification_dates = np.empty(max_nb_notes, dtype='float64')
-    #     new_modification_dates[note['ID']] = note['ModDate']
-    #
-    # with h5py.File(old_mod_dates_file, 'w') as f:
-    #     f['data'] = new_modification_dates
     print("Done")
+
+
+def get_current_backlinks(backlinks_text):
+    re_links = re.compile(r"\[\[.+\]\]")
+    # Find all links:
+    matches = re_links.findall(backlinks_text)
+    # Remove brackets and return:
+    return [match.replace('[[', '').replace(']]', '') for match in matches]
 
 
 def get_all_notes():
@@ -124,6 +125,7 @@ def get_all_notes():
                   WHERE TNote.ZTRASHED = 0\
                ORDER BY TNote.Z_PK DESC"
         return conn.execute(query)
+
 
 def get_notes_linking_to(id):
     with sqlite3.connect(bear_db) as conn:
@@ -142,6 +144,7 @@ def get_notes_linking_to(id):
                ORDER BY SNote.ZMODIFICATIONDATE ASC" % id
         return conn.execute(query)
 
+
 def get_note_links_in(id):
     with sqlite3.connect(bear_db) as conn:
         conn.row_factory = sqlite3.Row
@@ -155,14 +158,17 @@ def get_note_links_in(id):
                ORDER BY SNote.ZCREATIONDATE ASC" % id
         return conn.execute(query)
 
+
 def update_note(uid, new_text):
     x_command = 'bear://x-callback-url/add-text?id=' + uid +'&mode=replace_all&open_note=no&exclude_trashed=no&new_window=no&show_window=no&edit=no&timestamp=no'
     x_callback(x_command, new_text)
+
 
 def x_callback(x_command, md_text):
     x_command_text = x_command + '&text=' + urllib.parse.quote(md_text.encode('utf8'))
     subprocess.call(["open", "-g", x_command_text])
     time.sleep(.2)
+
 
 if __name__ == '__main__':
     main()
